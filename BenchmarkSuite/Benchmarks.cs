c44d971e8269d708c30e810e5a26422c78289abc
Aleksandr <->
2026-01-14 19:19:18 +0100

    benchmarks

---
+++ BenchmarkSuite/Benchmarks.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Application;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Loggers;
+
+namespace BenchmarkSuite
+{
+    [SimpleJob(RunStrategy.ColdStart, iterationCount: 3)]
+    [MemoryDiagnoser]
+    [ThreadingDiagnoser]
+    [ExceptionDiagnoser]
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void GenerateDefaultFile()
+        {
+            FileGeneratingOrchestrator.StartGenerating(); ;
+        }
+
+        [Benchmark]
+        public async Task SortDefaultFile()
+        {
+            await FileSortingOrchestrator.StartSorting();
+        }
+    }
+}
